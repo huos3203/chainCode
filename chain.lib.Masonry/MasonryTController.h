763bdc8dfe3fadc48f8af099c78f12e88a9b2683
huos3203 <724987481@qq.com>
2018-11-11 14:59:59 +0800

    masonrydemo链式编程思想

---
+++ chain.lib.Masonry/MasonryTController.h
@@ -0,0 +1,17 @@
+//
+//  MasonryTController.h
+//  chain.lib.Masonry
+//
+//  Created by admin on 2018/11/11.
+//  Copyright © 2018年 huoshuguang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MasonryTController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ chain.lib.Masonry/MasonryTController.m
@@ -0,0 +1,35 @@
+//
+//  MasonryTController.m
+//  chain.lib.Masonry
+//
+//  Created by admin on 2018/11/11.
+//  Copyright © 2018年 huoshuguang. All rights reserved.
+//
+
+#import "MasonryTController.h"
+#import <Masonry/Masonry.h>
+@interface MasonryTController ()
+
+@end
+
+@implementation MasonryTController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.view.backgroundColor = [UIColor whiteColor];
+    UIView *redView = [UIView new];
+//    redView.mas_top
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
