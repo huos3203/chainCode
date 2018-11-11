341f9ed63b81c7818b2e8222abc41a287afbd1a9
huos3203 <724987481@qq.com>
2018-11-11 13:53:17 +0800

    链式编程

---
+++ chain.lib.Masonry/chain_lib_Masonry.h
@@ -0,0 +1,19 @@
+//
+//  chain_lib_Masonry.h
+//  chain.lib.Masonry
+//
+//  Created by admin on 2018/11/11.
+//  Copyright © 2018年 huoshuguang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for chain_lib_Masonry.
+FOUNDATION_EXPORT double chain_lib_MasonryVersionNumber;
+
+//! Project version string for chain_lib_Masonry.
+FOUNDATION_EXPORT const unsigned char chain_lib_MasonryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <chain_lib_Masonry/PublicHeader.h>
+
+
---
+++ chain.lib.getblock/chain_lib_getblock.h
@@ -0,0 +1,19 @@
+//
+//  chain_lib_getblock.h
+//  chain.lib.getblock
+//
+//  Created by admin on 2018/11/11.
+//  Copyright © 2018年 huoshuguang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for chain_lib_getblock.
+FOUNDATION_EXPORT double chain_lib_getblockVersionNumber;
+
+//! Project version string for chain_lib_getblock.
+FOUNDATION_EXPORT const unsigned char chain_lib_getblockVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <chain_lib_getblock/PublicHeader.h>
+
+
